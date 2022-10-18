65fc7b48c146f9d8aeb1f83aafde1a3b5df9bb97
D3VS3C-Github <devsec.business@gmail.com>
2022-10-18 01:04:21 +0100

    [Optimized] code for readability

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import 'bootstrap';
 import './scss/style.scss';
 
 // Dark/Light Theme Switch Logic
-let mode = localStorage.getItem("mode");
+let mode     = localStorage.getItem("mode");
 var checkbox = document.querySelector('input[name=theme]');
+
 if (mode === "darkTheme") {
   checkbox.checked = true
   document.documentElement.setAttribute('data-theme', 'dark')
@@ -12,8 +13,11 @@ if (mode === "darkTheme") {
   checkbox.checked = false
   document.documentElement.setAttribute('data-theme', 'light')
 }
+
 mode = localStorage.getItem("mode");
+
 checkbox.addEventListener('change', function() {
+
   if (this.checked) {
     localStorage.setItem("mode", "darkTheme")
     trans()
@@ -23,7 +27,9 @@ checkbox.addEventListener('change', function() {
     trans()
     document.documentElement.setAttribute('data-theme', 'light')
   }
+
 })
+
 let trans = () => {
   document.documentElement.classList.add('transition');
   window.setTimeout(() => {
